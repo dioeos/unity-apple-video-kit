5cf89fb35e4aed1ac1c9af3826ae523f1ad17098
Ryan Dioneda <er.dioneda@gmail.com>
2026-05-01 12:55:39 -0400

    fix: Fixed import order conflict with UnityFramework-Swift header file and Corelocation

--- Plugins/iOS/Core/Coordinator/Coordinator.mm
+++ Plugins/iOS/Core/Coordinator/Coordinator.mm
@@ -1,15 +1,14 @@
 #import "Coordinator.h"
 #import <Foundation/Foundation.h>
 #import <ARKit/ARKit.h>
+#import <CoreLocation/CoreLocation.h>
 #import <stdlib.h>
 #import <string.h>
 
 #if __has_include(<UnityFramework/UnityFramework-Swift.h>)
-#import <CoreLocation/CoreLocation.h>
 #import <UnityFramework/UnityFramework-Swift.h>
 #else
 #import "UnityFramework-Swift.h"
-#import <CoreLocation/CoreLocation.h>
 #endif
 
 static ARCameraPoseBox *MakePoseBox(ARCameraPoseNative pose)
